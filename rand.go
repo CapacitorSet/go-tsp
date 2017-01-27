b6b46d4390ac190ed636b31c107cc7d2fc2edb75
Capacitor Set <capacitorset@gmail.com>
2017-01-27 11:10:38 +0100

    Various fixes

--- rand.go
+++ rand.go
@@ -19,6 +19,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"sort"
 )
 
@@ -37,55 +38,25 @@ type Edge struct {
 	Length     int
 }
 
-type graph []Edge
+type Graph []Edge
 
-func (g graph) Len() int {
+func (g Graph) Len() int {
 	return len(g)
 }
 
-func (g graph) Swap(i, j int) {
+func (g Graph) Swap(i, j int) {
 	g[i], g[j] = g[j], g[i]
 }
 
-func (g graph) Less(i, j int) bool {
+func (g Graph) Less(i, j int) bool {
+	if i >= len(g) || j >= len(g) {
+		fmt.Printf("graph_of_%d[%d/%d]\n", len(g), i, j)
+	}
 	return g[i].Length < g[j].Length
 }
 
-func main() {
-	r := rand.New(rand.NewSource(0))
-	noOfItems := 2 * 3 // 2 * num. palline
-	vertices := make([]Vertex, noOfItems)
-	var edges, graph, graph_temp graph
-
-	for i := 0; i < noOfItems; {
-		X := r.Intn(500)
-		Y := r.Intn(500 - 40)
-		Colour := Colour(r.Intn(3))
-		vertices[i] = Vertex{i, X, Y, Colour, 0}
-		i++
-
-		X = r.Intn(500)
-		Y = 500 - 20
-		vertices[i] = Vertex{i, X, Y, Colour, 1}
-		i++
-	}
-
-	for i, da := range vertices {
-		for j, a := range vertices {
-			if da.Kind == a.Kind {
-				continue
-			}
-			if da.Kind == 0 && (da.Colour != a.Colour) {
-				continue
-			}
-			if da == a {
-				continue
-			}
-
-			length := r.Intn(100)
-			edges = append(edges, Edge{&(vertices[i]), &(vertices[j]), length})
-		}
-	}
+func findShortestPath(vertices []Vertex, edges Graph) Graph {
+	var graph, graph_temp Graph
 
 	sort.Sort(edges)
 
@@ -136,19 +107,84 @@ func main() {
 			}
 		}
 		if doesCycle {
-			break
+			continue
 		}
-
 		graph = graph_temp
 	}
 
+	return graph
+}
+
+func printGraph(graph Graph) {
 	length := 0
 	for _, edge := range graph {
-		// fmt.Printf("%d -> %d\n", (*edge.This).ID, (*edge.Next).ID)
+		fmt.Printf("%d -> %d\n", (*edge.This).ID, (*edge.Next).ID)
 		length += edge.Length
 	}
 	fmt.Printf("%d edges, length %d\n", len(graph), length)
-	if len(graph) < (len(vertices) - 1) {
-		fmt.Print("Warning: output graph is shorter than expected\n")
+}
+
+// Returns whether the graph is proper or not
+func evaluateGraph(graph Graph, vertices []Vertex) bool {
+	if len(graph) < len(vertices) - 1 {
+		fmt.Printf("Output graph is shorter than expected: got %d, want %d\n", len(graph), len(vertices) - 1)
+		return false
+	} else {
+		fmt.Print("Output graph length is ok.\n")
+		return true
+	}
+}
+
+func main() {
+	r := rand.New(rand.NewSource(0))
+	noOfItems := 2 * 45 // 2 * num. palline
+	vertices := make([]Vertex, noOfItems)
+	var edges Graph
+
+	for i := 0; i < noOfItems; {
+		X := r.Intn(500)
+		Y := r.Intn(500 - 40)
+		Colour := Colour(r.Intn(3))
+		vertices[i] = Vertex{i, X, Y, Colour, 0}
+		i++
+
+		X = r.Intn(500)
+		Y = 500 - 20
+		vertices[i] = Vertex{i, X, Y, Colour, 1}
+		i++
+	}
+
+	for i, da := range vertices {
+		for j, a := range vertices {
+			if da.Kind == a.Kind {
+				continue
+			}
+			if da.Kind == 0 && (da.Colour != a.Colour) {
+				continue
+			}
+			if da == a {
+				continue
+			}
+
+			length := r.Intn(100)
+			edges = append(edges, Edge{&(vertices[i]), &(vertices[j]), length})
+		}
+	}
+
+	graph := findShortestPath(vertices, edges)
+
+	if evaluateGraph(graph, vertices) {
+		os.Exit(0)
+	}
+
+	for i := 0; i < len(edges); i++ {
+		// fmt.Printf("Blacklisting %d -> %d\n", (*edges[i].This).ID, (*edges[i].Next).ID)
+		// http://stackoverflow.com/a/37359662
+		edgeset := append(edges[:i], edges[i+1:]...)
+		graph = findShortestPath(vertices, edgeset)
+		if evaluateGraph(graph, vertices) {
+			printGraph(graph)
+			os.Exit(0)
+		}
 	}
 }
